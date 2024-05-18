7ab7c062c3603630ab7df5a7d2316db694cd8154
noriaki_tanimichi <tanimichi@morality-line.com>
2024-05-19 01:12:02 +0900

    ThrottleFirst

---
+++ Assets/Scripts/R3ThrottleFirstTest.cs
@@ -0,0 +1,17 @@
+using R3;
+using R3.Triggers;
+using System;
+using UnityEngine;
+
+public class R3ThrottleFirstTest : MonoBehaviour
+{
+	void Start()
+	{
+		this.UpdateAsObservable()
+			.Where(_ => Input.GetKeyDown(KeyCode.Space))
+			.ThrottleFirst(TimeSpan.FromSeconds(2))
+			.Subscribe(_ => Debug.Log("クリックされた！"));
+	}
+
+
+}
